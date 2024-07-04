b2a3f63fbcfabfb772ad7da31beba67e899b07a1
Kathir-23 <kathirkrish100@gmail.com>
2024-07-04 18:57:42 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,11 @@ public class Main {
         System.out.println(c);}
      static void add (int a, int b,int c){
         int d= a+b+c;
-        System.out.println(d);
-    }
+        System.out.println(d);}
+    static void add (int a,int c){
+        int d= a+c;
+        System.out.println(d);}
+
     public static void main(String[] args)
     {
         add(10, 5);
