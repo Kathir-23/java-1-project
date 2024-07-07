f133c279fc90d140539cdecca096489058370610
Kathir-23 <kathirkrish100@gmail.com>
2024-07-07 10:13:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,18 @@
-public class Main {
-
-    static void add (int a, int b){
-        int c= a+b;
-        System.out.println(c);}
-     static void add (int a, int b,int c){
-        int d= a+b+c;
-        System.out.println(d);}
-    static void add (int a,int c){
-        int d= a+c;
-        System.out.println(d);}
-    static void add (int a,int d,int f,int g){
-        int c= a+d+f+g;
-        System.out.println(d);}
-    for(int i=1;i<=5;i++)
-
-    public static void main(String[] args)
-    {
-        add(10, 5);
-        add(5,6,7);
-        add(1,3,7,4);
+public class Main
+ {
+   static int fact=1;
+static int factorial(int n)
+{
+    for(int i=1;i<=n;i++){
+       fact= fact*i;
+      
     }
+    return fact;
+    
+
 }
+public static void main(String[] args) {
+    factorial(8);
+ 
+    System.out.println(fact);
+}}
