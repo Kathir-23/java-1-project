53733874082a29e9d58ad5399b1b3269c4bafc0e
Kathir-23 <kathirkrish100@gmail.com>
2024-07-08 15:22:47 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,10 @@
 public class Main
  {
-   static int fact=1;
-static int factorial(int n)
-{
-    for(int i=1;i<=n;i++){
-       fact= fact*i;
       
-    }
-    return fact;
-    
-
-}
 public static void main(String[] args) {
-    factorial(8);
- 
-    System.out.println(fact);
+   for(int i=1;i<=n;i++){
+     for(int j=1;j<=n;j++){
+       
+      System.out.print("*");
+    }System.out.println(" ");}
 }}
