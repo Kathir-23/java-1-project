106539ab53053ac3ea4615bb79edc875ba7151cb
Kathir-23 <kathirkrish100@gmail.com>
2024-07-09 15:50:31 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public static void main(String[] args) {
    for(int i=1;i<=n;i++){
      for(int j=1;j<=n;j++){
        
-      System.out.print("*");
+      System.out.print("* ");
     }System.out.println(" ");}
 }}
